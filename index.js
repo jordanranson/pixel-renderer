e91f7542c70c39e280593f7cbc45e1ff75f069a2
Jordan Ranson <jordanranson@gmail.com>
2019-07-01 20:19:03 -0600

    Texture rendering

--- index.js
+++ index.js
@@ -2,38 +2,39 @@ class Renderer {
   get vertexShader () {
     return `
       attribute vec2 aVertexPosition;
-      attribute vec4 aVertexColor;
-      // attribute vec2 aTextureCoord;
+      attribute vec2 aTextureCoord;
 
-      // uniform vec2 uScalingFactor;
-
-      // varying highp vec2 vTextureCoord;
-      varying lowp vec4 vColor;
+      varying highp vec2 vTextureCoord;
 
       void main() {
         gl_Position = vec4(aVertexPosition, 0.0, 1.0);
-        // vTextureCoord = aTextureCoord;
-        vColor = aVertexColor;
+        vTextureCoord = aTextureCoord;
       }
     `
   }
 
   get fragmentShader () {
     return `
-      #ifdef GL_ES
-        precision highp float;
-      #endif
+      varying highp vec2 vTextureCoord;
 
-      //varying highp vec2 vTextureCoord;
-
-      //uniform sampler2D uSampler;
-      varying lowp vec4 vColor;
+      uniform sampler2D uScreenSampler;
+      // uniform sampler2D uPaletteSampler;
 
       void main() {
-        // gl_FragColor = texture2D(uSampler, vTextureCoord);
-        gl_FragColor = vColor;
+        // float index = texture2D(uScreenSampler, vTextureCoord.x).r;
+        // float color = texture2D(uPaletteSampler, index);
+
+        gl_FragColor = texture2D(uScreenSampler, vTextureCoord);
       }
     `
+  }
+
+  get width () {
+    return this.glCanvas.width
+  }
+
+  get height () {
+    return this.glCanvas.height
   }
 
   constructor ({ glCanvas, draw }) {
@@ -50,14 +51,28 @@ class Renderer {
     this.buildShaderProgram()
 
     // Create drawing surface
-    this.createDrawingSurface()
+    this.createScreenSurface()
 
     // Set screen size
     this.resize()
   }
 
-  run () {
-    this.render()
+  start () {
+    if (this.running) return
+
+    const renderLoop = () => {
+      this.draw()
+      this.render()
+      requestAnimationFrame(renderLoop)
+    }
+
+    this.running = true
+
+    renderLoop()
+  }
+
+  pause () {
+    this.running = false
   }
 
   render () {
@@ -67,41 +82,91 @@ class Renderer {
     this.clear()
 
     // Update array buffers
-    this.setArrayBuffer(2, 'aVertexPosition', this.drawingSurface.position)
-    this.setArrayBuffer(4, 'aVertexColor', this.drawingSurface.color)
+    this.setArrayBuffer(2, 'aVertexPosition', this.screenSurface.position)
+    this.setArrayBuffer(2, 'aTextureCoord', this.screenSurface.texture)
+
+    // Bind the surface texture
+    this.setTexture(this.screenTexture, this.width, this.height, this.screenBuffer)
+
+    // Set shader uniforms
+    this.setUniform1i('uScreenSampler', 0)
+    this.setUniform1i('uPaletteSampler', 1)
 
     // Draw surface
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
   }
-
-  // Drawing
 
   clear () {
     this.gl.clear(this.gl.COLOR_BUFFER_BIT)
   }
 
   resize () {
-    this.gl.viewport(0, 0, this.glCanvas.width, this.glCanvas.height)
+    this.createScreenTexture()
+    this.gl.viewport(0, 0, this.width, this.height)
   }
 
-  createDrawingSurface () {
+  createTexture (width = 1, height = 1, pixels = new Uint8Array([ 0, 0, 255, 255 ])) {
+    const { gl } = this
+
+    const texture = gl.createTexture()
+
+    this.setTexture(texture, width, height, pixels)
+
+    return texture
+  }
+
+  setTexture (texture, width, height, pixels) {
+    const { gl } = this
+
+    gl.bindTexture(gl.TEXTURE_2D, texture)
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, pixels)
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+  }
+
+  createScreenSurface () {
     const { gl } = this
 
     const position = this.createArrayBuffer([
-       1.0,  1.0,
-      -1.0,  1.0,
-       1.0, -1.0,
-      -1.0, -1.0,
+      -1.0, 1.0,
+       1.0, 1.0,
+      -1.0,  -1.0,
+       1.0,  -1.0
     ])
 
-    const color = this.createArrayBuffer([
-      1.0, 1.0, 1.0, 1.0,
-      1.0, 0.0, 0.0, 1.0,
-      0.0, 1.0, 0.0, 1.0,
-      0.0, 0.0, 1.0, 1.0
+    const texture = this.createArrayBuffer([
+      0.0, 0.0,
+      1.0, 0.0,
+      0.0, 1.0,
+      1.0, 1.0
     ])
 
-    this.drawingSurface = { position, color }
+    this.screenSurface = {
+      position,
+      texture
+    }
+  }
+
+  createScreenTexture () {
+    this.createScreenBuffer()
+    this.screenTexture = this.createTexture(this.width, this.height, this.screenBuffer)
+  }
+
+  createScreenBuffer () {
+    const length = this.width*this.height*4
+    const screenBuffer = new Uint8Array(length)
+
+    for (let i = 0; i < length; i += 4) {
+      screenBuffer[i]   = 0
+      screenBuffer[i+1] = 0
+      screenBuffer[i+2] = 255
+      screenBuffer[i+3] = 255
+    }
+
+    this.screenBuffer = screenBuffer
   }
 
   // Shader methods
@@ -164,5 +229,19 @@ class Renderer {
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
     gl.vertexAttribPointer(location, numComponents, gl.FLOAT, false, 0, 0)
     gl.enableVertexAttribArray(location)
+  }
+
+  // Uniforms
+
+  getUniformLocation (key) {
+    return this.gl.getUniformLocation(this.shaderProgram, key)
+  }
+
+  setUniform (type, key, value) {
+    this.gl[`uniform${type}`](this.getUniformLocation(key), value)
+  }
+
+  setUniform1i (key, value) {
+    this.setUniform('1i', key, value)
   }
 }
